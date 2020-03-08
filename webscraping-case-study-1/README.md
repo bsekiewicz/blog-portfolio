0df3776c89579b64fcdab1cd643e659bc70316dc
Bartosz Sękiewicz <b.sekiewicz@hta.pl>
2020-03-08 13:28:24 +0100

    rm image resize

--- webscraping-case-study-1/README.md
+++ webscraping-case-study-1/README.md
@@ -1,3 +1,3 @@
-# WebScraping && OCR
+# WebScraping, R and OCR
 
 Sources: [PL](http://www.worksmarter.pl/post/webscraping-case-study-1/) | [EN](http://www.worksmarter.pl/en/post/webscraping-case-study-1/)
--- webscraping-case-study-1/code.R
+++ webscraping-case-study-1/code.R
@@ -1,28 +1,29 @@
+# packages
 require(magrittr) # pipe
-require(rvest) # webscraping
+require(rvest) # web scraping
 require(stringi) # text cleaning
 require(readr) # file reading
-require(magick) #  image processing 
+require(magick) # image processing 
 require(tesseract) # OCR
-require(googledrive) # gdrive API
+require(googledrive) # Google Drive
 
-
-# download data
-
+# download webpage
 s <- paste0( 
   'https://www.analizy.pl/',
   'fundusze/fundusze-inwestycyjne/notowania') %>%
   html_session
 
+# data extraction
 notowania <- s %>%
+  # noteTable is the only element on the page
   html_node("#noteTable") %>% 
-  # header has two levels
+  # the header has two levels
   html_table(header = TRUE) %>% 
   .[,-1] %>% 
   set_colnames(.[1,]) %>% 
-  # first column is ommited (its image)
+  # the first column is omitted (its image)
   .[-1,] %T>% 
-  # show summary
+  # summary
   str
 
 # get links
@@ -33,26 +34,33 @@ notowania$`j.u. netto` <- s %>%
 
 # tesseract
 
-# engine configuration
+# OCR settings
 engine <-
-  tesseract(options =
-              list(tessedit_char_whitelist = " 0123456789,.",
-                   tessedit_char_blacklist = "!?@#$%&*()<>_-+=/:;'\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\n\t\r",
-                   classify_bln_numeric_mode = "1"))
+  tesseract(options = 
+    list(tessedit_char_whitelist = " 0123456789,.",
+         tessedit_char_blacklist = "!?@#$%&*()<>_-+=/:;'\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\n\t\r",
+         classify_bln_numeric_mode = "1"))
 
-# sample results
+# text cleaning
+text_clearing <- function(x) {
+  x %>%
+    stri_replace_all_regex("[ \n]", "") %>%
+    stri_replace_all_regex("[,]+", ".") %>%
+    stri_extract_first_regex('[0-9]+[//.]{0,1}[0-9]{0,2}') %>%
+    as.numeric %>%
+    format(2)
+}
+
+# sample conversion
 notowania[['j.u. netto']][1] %>%
   image_read %>%
-  # trial and error
-  image_resize("130") %>%
   ocr(engine) %>%
-  stringi::stri_replace_all_regex("[ \n]", "") %>%
-  as.numeric
+  text_clearing
 
 # google drive
 
+# function
 OCRbyGoogleDrive <- function(urls) {
-  
   # download images
   imgs <- list()
   for (url in urls) {
@@ -61,35 +69,34 @@ OCRbyGoogleDrive <- function(urls) {
                        fn  = tempfile())))
   }
   lapply(imgs, function(x)
-  {image_read(x$url) %>%
-      image_resize("130") %>%
+    {image_read(x$url) %>%
       image_write(x$fn, format = "png")})
-  
-  # send images to google drive
+
+  # send images to Google Drive
   gd_imgs <-
     lapply(imgs, function(x)
-    {drive_upload(x$fn, type = "png")})
-  
-  # convert to google documents
+      {drive_upload(x$fn, type = "png")})
+
+  # convert to Google documents
   gd_imgs_cp <-
     lapply(gd_imgs, function(x)
-    {drive_cp(as_id(x$id),
-              mime_type = drive_mime_type("document"))})
-  
+      {drive_cp(as_id(x$id),
+                mime_type = drive_mime_type("document"))})
+
   # write as txt files
   txt_files <-
     lapply(gd_imgs_cp, function(x)
-    {drive_download(as_id(x$id), type = "txt")})
-  
+      {drive_download(as_id(x$id), type = "txt")})
+
   # text cleaning
   res <-
     sapply(txt_files, function(x)
-    {readr::read_file(x$local_path)}) %>%
-    stringi::stri_replace_all_regex("[ ,\\.]", "") %>%
-    stringi::stri_extract_last_regex("[0-9]+") %>%
+      {read_file(x$local_path)}) %>%
+    stri_replace_all_regex("[ ,\\.]", "") %>%
+    stri_extract_last_regex("[0-9]+") %>%
     as.numeric %>%
     divide_by(100)
-  
+
   # remove temporary files
   imgs %>% lapply(function(x)
     unlink(x$fn))
@@ -99,11 +106,9 @@ OCRbyGoogleDrive <- function(urls) {
     drive_rm(as_id(x$id)))
   gd_imgs_cp %>% lapply(function(x)
     drive_rm(as_id(x$id)))
-  
+
   return(cbind(urls, res))
 }
 
-# sample results
-
-# google authentication is needed
-OCRbyGoogleDrive(notowania[['j.u. netto']][1])
+# sample conversion
+OCRbyGoogleDrive(notowania[['j.u. netto']][1]) # google authentication is needed
